f79db7395fb5357e9f50ebd2f3959b0804e6711b
sghzal <sghzal@ADM2-PC02>
2013-04-05 14:21:24 +0200

    Commit

--- lib/Peek/StringUtils.php
+++ lib/Peek/StringUtils.php
@@ -5,7 +5,7 @@ namespace Peek;
 /**
  * Description of StringUtils
  *
- * @author bobito
+ * @author Soufiane Ghzal
  */
 class StringUtils {
     
@@ -15,15 +15,41 @@ class StringUtils {
      * @return string the cleaned string
      */
     public static function stripCharExtra($haystack){
-        return strtr($haystack,
-                'àáâãäèéêëìíîïñòóôõöùúûüýÿÀÁÂÃÄÇÈÉÊËÌÍÎÏÑÒÓÔÕÖÙÚÛÜÝç',
-                'aaaaaeeeeiiiinooooouuuuyyAAAAACEEEEIIIINOOOOOUUUUYc'
-        );
+            return str_replace(
+                    array(
+                            'à', 'â', 'ä', 'á', 'ã', 'å',
+                            'î', 'ï', 'ì', 'í', 
+                            'ô', 'ö', 'ò', 'ó', 'õ', 'ø', 
+                            'ù', 'û', 'ü', 'ú', 
+                            'é', 'è', 'ê', 'ë', 
+                            'ç', 'ÿ', 'ñ',
+                            'À', 'Â', 'Ä', 'Á', 'Ã', 'Å',
+                            'Î', 'Ï', 'Ì', 'Í', 
+                            'Ô', 'Ö', 'Ò', 'Ó', 'Õ', 'Ø', 
+                            'Ù', 'Û', 'Ü', 'Ú', 
+                            'É', 'È', 'Ê', 'Ë', 
+                            'Ç', 'Ÿ', 'Ñ', 
+                    ),
+                    array(
+                            'a', 'a', 'a', 'a', 'a', 'a', 
+                            'i', 'i', 'i', 'i', 
+                            'o', 'o', 'o', 'o', 'o', 'o', 
+                            'u', 'u', 'u', 'u', 
+                            'e', 'e', 'e', 'e', 
+                            'c', 'y', 'n', 
+                            'A', 'A', 'A', 'A', 'A', 'A', 
+                            'I', 'I', 'I', 'I', 
+                            'O', 'O', 'O', 'O', 'O', 'O', 
+                            'U', 'U', 'U', 'U', 
+                            'E', 'E', 'E', 'E', 
+                            'C', 'Y', 'N', 
+                    ),$haystack);
     }
     
     /**
      * remove all non-alphanumeric chars from the given string and return a clean string
      * @param string $haystack the string to be cleaned
+     * @param boolean $removeSpaces true if spaces have to been removed too. Default to true
      * @return string the clean string
      */
     public static function onlyAlphaNum($haystack,$removeSpaces=true){
@@ -33,7 +59,7 @@ class StringUtils {
     }
     
     /**
-     * peek1 is a format for search engine where all chars are only alphanumeric and lower chars. All accents are converted before alphnum filter applies
+     * Peek1 is a format for search engine where all chars are only alphanumeric and lower chars. All accents are converted before alphnum filter applies
      * @param type $newStr
      * @return type
      */
@@ -41,6 +67,34 @@ class StringUtils {
         $newStr = StringUtils::stripCharExtra($newStr);
         $newStr = strtolower($newStr);
         $newStr = StringUtils::onlyAlphaNum($newStr);
+        
+        return $newStr;
+    }
+    
+    /**
+     * BPeek1 is made of multiple Peek1 , each surrounded by a dot [.]. Dot are converted from 
+     *  - comma [,]
+     *  - dot [.]
+     *  - colon [:]
+     *  - semicolon [;]
+     *  - pipe [|]
+     * note that spaces [ ] dashes [-] and underscore [_] are ignored and will be converted to Peek1
+     * 
+     * This format allows a strict sql search in a keyword list
+     * 
+     * e.g. : 
+     *      you have this BPeek1 keyword list :   .music.cd.acdc.
+     *      you want to find "cd"
+     *      you will do :  
+     *          my_column LIKE "%.cd.%"
+     *      Then you wont match with "acdc"
+     * 
+     * @param type $newStr
+     * @return type
+     */
+    public static function toBPeek1($newStr){ 
+        
+        // TODO : think more about this format
         
         return $newStr;
     }
